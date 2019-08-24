4fb9812d2e1ee6bae5a7a60917fb53dc5535d21e
shurjom <shurjom@gmail.com>
2019-08-23 23:13:14 -0400

    Initial Commit

---
+++ HitchIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitchIt
+//
+//  Created by shurjom on 8/23/19.
+//  Copyright © 2019 shurjom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
